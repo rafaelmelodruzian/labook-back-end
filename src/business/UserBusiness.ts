acf93f4e54a537e13ab6731956edac1e6dea397b
rafaelmelodruzian <rmdconsultor@gmail.com>
2023-08-14 17:13:28 -0300

    Implementado e configurado end-point de cadastro de usuario(signup)

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -2,6 +2,8 @@ import { HashManager } from "../services/HashManager";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
 import { UserDatabase } from "../database/UserDatabase";
+import { SignupInputDTO, SignupOutputDTO } from "../dtos/user/signup.dto";
+import { TokenPayload, USER_ROLES, User } from "../models/User";
 
 export class UserBusiness {
   constructor(
@@ -11,6 +13,40 @@ export class UserBusiness {
     private hashManager: HashManager
   ) {}
 
+  public signup = async (
+    input: SignupInputDTO
+  ): Promise<SignupOutputDTO> => {
+    const { name, email, password } = input
+    
+    const id = this.idGenerator.generate()
 
+    const hashedPassword = await this.hashManager.hash(password)
+    
+    const user = new User(
+      id,
+      name,
+      email,
+      hashedPassword,
+      USER_ROLES.NORMAL,
+      new Date().toISOString()
+    )
+
+    const userDB = user.toDBModel()
+    await this.userDatabase.insertUser(userDB)
+
+    const payload: TokenPayload = {
+      id: user.getId(),
+      name: user.getName(),
+      role: user.getRole()
+    }
+
+    const token = this.tokenManager.createToken(payload)
+
+    const output: SignupOutputDTO = {
+      token
+    }
+
+    return output
+  }
 
 }
